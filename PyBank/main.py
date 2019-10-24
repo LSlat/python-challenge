776737fbccc3dd35e1572c5bfcae4d5bdd5890e0
LSlat <49042469+LSlat@users.noreply.github.com>
2019-10-24 18:40:46 -0500

    Add files via upload

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,132 +1,88 @@
+# Dependencies
 import os
 import csv
 
+# File path
 csvpath = os.path.join("Resources", "budget_data.csv")
 
 # Lists to store data
-tot_change = []
-#net_change = []
-greatest_change = []
-least_change = []
+net_change_list = []
+greatest_increase = []
+greatest_decrease = []
 
-#net_change = 0
-tot_change = 0
+tot_months = 0
+tot_net = 0
+greatest_increase = 0
+greatest_decrease = 999999999
+month_increase = []
+month_decrease = []
+prev_net = 0
 
-greatest_change = 0
-least_change = 0
-
+# Read the data in the file
 with open(csvpath,newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
+    # Header row
     csv_header = next(csvreader)
-    #---This 2nd NEXT throws results off(?)
-    #firstrow = next(csvreader)
-    #firstvalue = int(firstrow[1])
-    #print(f"Header: {csv_header}")
 
-    #new_header = ("Financial Analysis")
-    #print(new_header)
-    #print("--------------------------")
+    # Get the first row of data
+    first_row = next(csvreader)
+    tot_months = tot_months + 1
+    tot_net = tot_net + int(first_row[1])
+    prev_net = prev_net + int(first_row[1])
 
-    #month_label = ("Total Months: ")
-    #avg_label = ("Average Change: ")
-    #max_inc_label = ("Greatest Increase in Profits: ")
-    #min_inc_label = ("Greatest Decrease in Profits: ")
-
+    # Loop through data
     for row in csvreader:
-        net_change = int(row[2])
-        previous = int(row[1])
+
+        # Totals
+        tot_months = tot_months + 1
+        tot_net = tot_net + int(row[1])
+
+        # Net amounts        
+        net_change = int(row[1]) - int(prev_net)
+
+        # Reset prev_net as loop through data
+        prev_net = int(row[1])
+
+        # Add the current net change to the list to calculate average
+        net_change_list = net_change_list + [net_change]
+        avg_net = round(sum(net_change_list) / len(net_change_list))
+
+        # Greatest increase in profits
+        if net_change > greatest_increase:
+            month_increase = row[0]
+            greatest_increase = net_change
+
+        # Greatest decrease in losses
+        if net_change < greatest_decrease:
+            month_decrease = row[0]
+            greatest_decrease = net_change
+
+        # Average net change
+        #avg_net = round(sum(net_change_list) / len(net_change_list))
         
-        #prev_change = int(row[2])
-        #if net_change > greatest_change:
-         #   greatest_change = net_change
-#print(greatest_change)
-        
-        
-        
-        #difference = int(row[1]) - firstvalue
-
-        #---All of these dates print with each difference
-        #Dates.append(row[0])
-        #Profit_Loss.append(row[1])
-        #difference.append(row[2])
-        
-
-        #tot_months = len(Dates)+1
-        #net = net + int(row[1])
-
-        #net = Profit_Loss.sum()
-        #net = sum(Profit_Loss)
-        #firstvalue = int(row[1])
-        #print(firstvalue)
-        #print(Dates)
-        #print(difference)
-
-        #total_diff = diff + int(row[1])
-
-        
-
-#---Gives wrong result     
-avg_diff = total_diff/months
-
-#---Gives error-'int' object is not iterable
-#max_diff = max(difference)
-#min_diff = min(difference)
+# Print results
 print(f"Financial Analysis")
 print("------------------------------")
-print(f"Total Months: {months}")
-print(f"Net Total: ")
-print(f"Average Change: $")
-print(f"Greatest Increase in Profits: ")
-print(f"Greatest Decrease in Profits: ")
+print(f"Total Months: {tot_months}")
+print(f"Net Total: ${tot_net}")
+print(f"Average Change: ${avg_net}")
+print(f"Greatest Increase in Profits: {month_increase}, ${greatest_increase}")
+print(f"Greatest Decrease in Losses: {month_decrease}, ${greatest_decrease}")
 
-
-#print(month_label + str(months))
-#print(avg_label + str(avg_diff))
-
-#print(max_diff)
-#print(min_inc_label)       
-#print(min_diff)
-#print(f"Total Months: ", months)
-
-
-
-# new_csv = zip(Dates, Profit_Loss)
-#new_csv = (months)
-
-# output_file = os.path.join("new_data.csv")
+# File to output
 output_file = os.path.join("Results.txt")
 
+# Write to text file
 with open("Results.txt", "w") as text_file:
     text_file.write(f"Financial Analysis\n")
     text_file.write("------------------------------\n")
-    text_file.write(f"Total Months: {months}\n")
-    text_file.write(f"Net Total: \n")
-    text_file.write(f"Average Change: $\n")
-    text_file.write(f"Greatest Increase in Profits: \n")
-    text_file.write(f"Greatest Decrease in Profits: \n")
+    text_file.write(f"Total Months: {tot_months}\n")
+    text_file.write(f"Net Total: ${tot_net}\n")
+    text_file.write(f"Average Change: ${avg_net}\n")
+    text_file.write(f"Greatest Increase in Profits: ${greatest_increase}\n")
+    text_file.write(f"Greatest Decrease in Profits: ${greatest_decrease}\n")
     
-    #writer = csv.writer
-
-
-    # writer.writerow(["Date", "Profit/Losses"])
-    #writer.writerow([new_header])
-    #writer.writerow(["----------------------------"])
-    #text_file.write(new_header)
-    
-
-    #---from https://stackoverflow.com/questions/5214578/python-print-string-to-text-file
-    #with open("Output.txt", "w") as text_file:
-    #text_file.write("Purchase Amount: %s" % TotalAmount)
-
-    #writer.writerow("Total Months: " )
-    #writer.writerow(f[Total Months: ] + str(months))
-    #writer.writerow(month_label, months)
-    #writer.writerow(month_label + str(months))
-    #text_file.write(month_label + str(months))
-
-#for row in new_csv:
-    #print(row)
-
+   
 
     
